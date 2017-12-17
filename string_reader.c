948bc05b2e7423a44301f26b009a12aba47dc668
Kasper Nissen <kaspernissen1997@gmail.com>
2017-12-17 16:38:25 +0100

    Sorting is now functional, need commenting and formatting

--- string_reader.c
+++ string_reader.c
@@ -1,65 +1,77 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "vector.h"
 
-/*
-int main (char *argc, char* argv[]) {
-    char const* const fileName = argv[1];
-    FILE* file = fopen(fileName, "r");
-    char line[256];
-    
-    Vector v;
-    vector_init(&v);
+/*void myArrSort(Vector *v) {
+    void data = vector_get_array(v);
 
-    while(fgets(line, sizeof(line), file)){
-        vector_push(&v, &line); 
+    for (int size = vector_size(v); size > 0; size--) {
+        data[size]
     }
+}*/
 
-    printf("%s", (char*) v.data[v.top]);
-    
-    while(vector_size(&v)){
-        printf("%s", (char*) vector_pop(&v));
-    }
-
-    fclose(file);
-
-    return 0;
+/* qsort C-string comparison function */ 
+int cstring_cmp(const void *a, const void *b) 
+{ 
+    const char **ia = (const char **)a;
+    const char **ib = (const char **)b;
+    return strcmp(*ia, *ib); 
 }
-*/
 
-int main(char *argc, char* argv[]) {
+int main(int *argc, char *argv[]) {
+    // Opens the file, asserts memory for the filebuffer and -
+    // prepares an address for the read files to be stored at
     FILE *pFile = fopen(argv[1], "r");
     size_t *buffer = (size_t) 0;
     char *line = NULL;
 
+    // Constructs the to-be-utilized vector
     Vector v;
+    // Stores the address of the newly constructed vector
     Vector *pv = &v;
 
+    // Initializes the newly constructed vector
     vector_init(pv);
 
     while ((getline(&line, &buffer, pFile)) != -1) {
-
-
         vector_push(pv, (void *) assignNewStringPointer(line));
-
-        printf("Getting element at [0]: %s", vector_get_element(pv, 0));
+        
+        // TODO: Delete this, meant for testing only
+        // printf("Getting element at [0]: %s", vector_get_element(pv, 0));
     }
 
+    // TODO: Delete this, meant for testing only
     for (int i = pv->top; i > 0; i--) {
-        printf("Popping: %s", (char *) vector_pop(pv));
+        printf("Element at [%i]: %s", i - 1, (char *) vector_get_element(pv, i - 1));
     }
-    
-    printf("Done mah nignog");
+
+    qsort(vector_get_array(pv), pv->top, sizeof(char *), cstring_cmp);
+
+    // TODO: Delete this, meant for testing only
+    for (int i = pv->top; i > 0; i--) {
+        printf("Element at [%i]: %s", i - 1, (char *) vector_get_element(pv, i - 1));
+    }
 
     fclose(pFile);
+
     if (line)
         free(line);
 
     return 0;
 }
 
+// Gives the string found at the given address its own address
 void assignNewStringPointer(char *line) {
+    // Asserts enough memory for the entirity of the next string to be read
+    // TODO: Explain why a single extra byte is added
     void *newLine = malloc(strlen(line) + 1);
+    // void *newLine = malloc(sizeof(char) * 256);
+
+    // Copies the contents of the 'line' address to the new address at 'newLine'
     memcpy(newLine, line, strlen(line));
+    // memcpy(newLine, line, sizeof(char) * 256);
+
+    // Returns the new address
     return newLine;
 }
